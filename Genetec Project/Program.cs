9822b65f2ea698de4a64a8a54c87ea664daf8745
Anis Brachemi <anisbrachemi@hotmail.com>
2021-03-08 09:45:28 -0500

    Add project files.

---
+++ Genetec Project/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+
+namespace Genetec_Project
+{
+
+    class Program
+    {
+        static string connectionString = "<NAMESPACE CONNECTION STRING>";
+        static string topicName = "<TOPIC NAME>";
+        static string subscriptionName = "<SUBSCRIPTION NAME>";
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+
+        static async Task SendMessageToTopicAsync() {
+            // create a Service Bus client 
+            await using (ServiceBusClient client = new ServiceBusClient(connectionString)) {
+                // create a sender for the topic
+                ServiceBusSender sender = client.CreateSender(topicName);
+                await sender.SendMessageAsync(new ServiceBusMessage("Hello, World!"));
+                Console.WriteLine($"Sent a single message to the topic: {topicName}");
+            }
+        }
+
+        static Queue<ServiceBusMessage> CreateMessages() {
+            // create a queue containing the messages and return it to the caller
+            Queue<ServiceBusMessage> messages = new Queue<ServiceBusMessage>();
+            messages.Enqueue(new ServiceBusMessage("First message"));
+            messages.Enqueue(new ServiceBusMessage("Second message"));
+            messages.Enqueue(new ServiceBusMessage("Third message"));
+            return messages;
+        }
+    }
+}
